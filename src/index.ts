628959613fdb5b14d27af8ac05502fefc8f1e447
YurL0014 <ylogachev-sfedu@mail.ru>
2023-08-24 11:04:54 +0300

    Added secure MW + profile endpoint + fixed error with Redis import

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,15 @@
-import express, {Request, Response} from 'express';
+import express, {NextFunction, Request, Response} from 'express';
 import session from 'express-session';
-import redis from 'redis';
+import * as redis from 'redis';
+
+
+// Разобраться как потом это перенести в отдельный файл
+declare module 'express-session' {
+    export interface SessionData {
+        clientId: string;
+    }
+}
+
 
 import RedisStore from "connect-redis"
 
@@ -54,7 +63,23 @@ app.post('/login', (req: Request, res: Response) => {
     res.json('you are now logged in')
 })
 
+//4 plug in another MW that will check if the user is authenticated or not
+// all requests are plugged in after this MW will only be accessible if the user is logged in
 
+app.use((req: Request, res: Response, next: NextFunction) => {
+    if (!req.session || !req.session.clientId) {
+        const err = new Error('Yoy shall not pass');
+        //@ts-ignore
+        err.statusCode = 401;
+        next(err);
+    }
+    next();
+});
+
+// 5 plug in all routes that the user can only access if logged in
+app.post('/profile', (req: Request, res: Response) => {
+    res.json(req.session)
+})
 
 app.get('/', (req: Request, res: Response) => {
     res.send('Hello World!')
